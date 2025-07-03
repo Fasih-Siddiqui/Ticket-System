7c6a9c4f2014399196ccc01b7271d50021b4dcd4
Nuxhat <nuxhat.syed@gmail.com>
2025-07-03 11:54:42 +0500

    Update page.js

--- client/app/dashboard-employee/page.js
+++ client/app/dashboard-employee/page.js
@@ -796,9 +796,9 @@ export default function AdminDashboard() {
         </div>
 
         {/* Footer */}
-        <div className="w-full bg-gradient-to-r from-blue-100 via-blue-400 to-gray-600 shadow-lg text-white py-2 text-center">
+        {/* <div className="w-full bg-gradient-to-r from-blue-100 via-blue-400 to-gray-600 shadow-lg text-white py-2 text-center">
           <p>&copy; {new Date().getFullYear()} i-MSConsulting | All rights reserved. Designed by i-MSConsulting.</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
