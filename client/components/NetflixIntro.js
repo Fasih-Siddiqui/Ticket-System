9045ba04cf697228481efbbeb95f22a213ab57a8
Nuxhat <nuxhat.syed@gmail.com>
2025-07-08 11:30:04 +0500

    splash

---
+++ client/components/NetflixIntro.js
@@ -0,0 +1,10 @@
+<div id="container">
+  <div class="netflixintro">
+    <div class="n-letter">
+      <div class="bar left-bar"></div>
+      <div class="bar right-bar"></div>
+      <div class="bar diagonal-bar"></div>
+    </div>
+    <div class="effect-brush"></div>
+  </div>
+</div>
